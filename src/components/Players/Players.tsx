c2318beb3cbfda3d2710fefe93a29fc8c69b331b
Gyanadewan <gyanadewan1@gmail.com>
2026-09-10 23:25:56 +0600

    add update button background based on active state

--- src/components/Players/Players.tsx
+++ src/components/Players/Players.tsx
@@ -1,25 +1,38 @@
 
 import type { Iplayer } from '../../player'
-import { use } from 'react'
+import { use, useState } from 'react'
 import AvailablePlayers from '../AvailablePlayers/AvailablePlayers'
 interface PlayerProps {
   playersDataFetch: Promise <Iplayer[]>
 }
 function Players({playersDataFetch}:PlayerProps) {
    const players = use(playersDataFetch)
+
+   const [buttonType,setButtonType] = useState("available")
   
+   const  setButtonHandler = (type:string)=> {
+       setButtonType(type)
+   }
   return (
      <div className='container mx-auto py-4'>
       <div className=' flex justify-between'>
          <h1 className='font-semibold text-2xl'>Available Players</h1>
           
-  <div className="flex gap-3">
-  <button className="bg-yellow-400 px-5 py-2 rounded-lg font-semibold">
+  <div className="flex ">
+  <button onClick={()=> setButtonHandler("available")} className={`px-4 py-2 rounded-2xl rounded-r-none ${
+            buttonType === "available"
+              ? "bg-yellow-400"
+              : "bg-gray-200"
+          }`}>
     Available
   </button>
 
-  <button className="bg-gray-200 px-5 py-2 rounded-lg font-semibold">
-    Selected
+  <button onClick={()=> setButtonHandler("selected")} className={`px-4 py-2 rounded-2xl rounded-l-none ${
+            buttonType === "selected"
+              ? "bg-yellow-400"
+              : "bg-gray-200"
+          }`}>
+    Selected 
   </button>
 </div>
         </div> 
